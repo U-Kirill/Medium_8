f57f4c482e23ec72200e2e64304d015b03fcaafd
kglad67@gmail.com <PRo1001111Xo309p>
2019-09-02 23:14:58 +0300

    Task 2 Complited

--- Lesson1/ConsoleApp/ConsoleApp/Program.cs
+++ Lesson1/ConsoleApp/ConsoleApp/Program.cs
@@ -8,8 +8,45 @@ namespace ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            List<Object> objects = new List<Object>();
+
+            objects.Add(new Object(5, 5, true));
+            objects.Add(new Object(10, 10, true));
+            objects.Add(new Object(15, 15, true));
+
+
+            while (true)
+            {
+                CheckForCollide(objects[1], objects[2]);
+                CheckForCollide(objects[1], objects[3]);
+                CheckForCollide(objects[2], objects[3]);
+
+                for (int i = 0; i < objects.Count; i++)
+                {
+                    objects[i].RandomMove();
+                    TryVsualize(objects[i], i);
+                }
+
+                void CheckForCollide(Object object1, Object object2)
+                {
+                    if (object1.Xpositions == object2.Xpositions && object1.Ypositions == object2.Ypositions)
+                    {
+                        object1.IsAlive = false;
+                        object1.IsAlive = false;
+                    }
+                }
+
+                void TryVsualize(Object _object, int index)
+                {
+                    if (_object.IsAlive)
+                    {
+                        Console.SetCursorPosition(_object.Xpositions, _object.Ypositions);
+                        Console.Write(index + 1);
+                    }
+                }
+            }
         }
     }
 }
