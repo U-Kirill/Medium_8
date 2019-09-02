dc6176f8a2430299107e0ad8eca085d9c72de91b
kglad67@gmail.com <PRo1001111Xo309p>
2019-09-02 22:24:37 +0300

    Task1 complited

---
+++ Lesson1/ConsoleApp/ConsoleApp/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class User
+    {
+        private int _id;
+        private string _name;
+        private int _salary;
+
+        User(int id, string name, int salary)
+        {
+            _id = id;
+            _name = name;
+            _salary = salary;
+        }
+
+        public int Id => _id;
+        public string Name => _name;
+        public int Salary => _salary;
+    }
+}
---
+++ Lesson1/ConsoleApp/ConsoleApp/Users.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Users
+    {
+        private List<User> _allUsers;
+
+        public List<User> AllUsers => _allUsers;
+
+
+        public void AddUser(User user)
+        {
+            _allUsers.Add(user);
+        }
+
+        public User GetUserById(int id)
+        {
+            return _allUsers.Find(x => x.Id == id);
+        }
+
+        public User GetUserByName(string name)
+        {
+            return _allUsers.Find(x => x.Name == name);
+        }
+
+        public List<User> GetUsersWhereMinSalary(int salary)
+        {
+            return _allUsers.FindAll(x => x.Salary >= salary);
+        }
+
+        public List<User> GetUsersWhereMaxSalary(int salary)
+        {
+            return _allUsers.FindAll(x => x.Salary <= salary);
+        }
+
+        public List<User> GetUsersWithSalaryBetween(int minSalary, int maxSalary)
+        {
+            return _allUsers.FindAll(x => x.Salary > minSalary && x.Salary < maxSalary);
+        }
+    }
+}
