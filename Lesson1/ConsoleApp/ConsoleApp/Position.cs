4e81aa3bb594fbc3797c908ee51e33587a907357
kglad67@gmail.com <PRo1001111Xo309p>
2019-09-08 02:44:55 +0300

    Transition from general class name "Obect" to more specific "ElementaryParticle".  Fix from nested functions. And other changes

---
+++ Lesson1/ConsoleApp/ConsoleApp/Position.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Position
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Position(int xPosition, int yPosition)
+        {
+            X = xPosition;
+            Y = yPosition;
+        }
+
+        public void Move(int x, int y)
+        {
+            X += x;
+            Y += y;
+        }
+
+        public void MakeNonNegative()
+        {
+            if (X < 0)
+                X = 0;
+
+            if (Y < 0)
+                Y = 0;
+        }
+    }
+}
--- Lesson1/ConsoleApp/ConsoleApp/Program.cs
+++ Lesson1/ConsoleApp/ConsoleApp/Program.cs
@@ -10,42 +10,37 @@ namespace ConsoleApp
     {
         public static void Main(string[] args)
         {
-            List<Object> objects = new List<Object>();
+            Random random = new Random();
+            List<ElementaryParticle> objects = new List<ElementaryParticle>();
 
-            objects.Add(new Object(5, 5, true));
-            objects.Add(new Object(10, 10, true));
-            objects.Add(new Object(15, 15, true));
+            objects.Add(new ElementaryParticle(5, 5, true));
+            objects.Add(new ElementaryParticle(10, 10, true));
+            objects.Add(new ElementaryParticle(15, 15, true));
 
 
             while (true)
             {
-                CheckForCollide(objects[1], objects[2]);
-                CheckForCollide(objects[1], objects[3]);
-                CheckForCollide(objects[2], objects[3]);
+                for (int i = 0; i < objects.Count - 1; i++)
+                {
+                    for (int j = i; j < objects.Count - 1; j++)
+                    {
+                        objects[i].Intersect(objects[j + 1]);
+                    }
+                }
 
                 for (int i = 0; i < objects.Count; i++)
                 {
-                    objects[i].RandomMove();
+                    objects[i].RandomMove(random);
                     TryVsualize(objects[i], i);
                 }
-
-                void CheckForCollide(Object object1, Object object2)
-                {
-                    if (object1.Xpositions == object2.Xpositions && object1.Ypositions == object2.Ypositions)
-                    {
-                        object1.IsAlive = false;
-                        object1.IsAlive = false;
-                    }
-                }
-
-                void TryVsualize(Object _object, int index)
-                {
-                    if (_object.IsAlive)
-                    {
-                        Console.SetCursorPosition(_object.Xpositions, _object.Ypositions);
-                        Console.Write(index + 1);
-                    }
-                }
+            }
+        }
+        static void TryVsualize(ElementaryParticle _object, int index)
+        {
+            if (_object.IsAlive)
+            {
+                Console.SetCursorPosition(_object.Position.X, _object.Position.Y);
+                Console.Write(index + 1);
             }
         }
     }
