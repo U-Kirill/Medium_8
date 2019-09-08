1246b6427d387396abf8478cc97aad241830ef09
kglad67@gmail.com <PRo1001111Xo309p>
2019-09-08 20:05:47 +0300

    Task Complited

---
+++ Lesson1/ConsoleApp/ConsoleApp/Human.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Human : Living
+    {
+        public int Agility { get; private set; }
+
+        public Human(int agility, int health) : base(health)
+        {
+            Agility = agility;
+        }
+
+        public override void TakeDamage(int damage)
+        {
+            base.TakeDamage(damage / Agility);
+        }
+    }
+}
---
+++ Lesson1/ConsoleApp/ConsoleApp/Wombat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Wombat : Living
+    {
+        public int Armor { get; private set; }
+
+        public Wombat(int armor, int health) : base(health)
+        {
+            Armor = armor;
+        }
+
+        public override void TakeDamage(int damage)
+        {
+            base.TakeDamage(damage - Armor);
+        }
+    }
+}
