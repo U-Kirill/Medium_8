e45dfc5128a053a4042b44dcb21a82d87eecd400
kglad67@gmail.com <PRo1001111Xo309p>
2019-09-08 17:08:04 +0300

    Correcting Name

--- Lesson1/ConsoleApp/ConsoleApp/Player.cs
+++ Lesson1/ConsoleApp/ConsoleApp/Player.cs
@@ -10,54 +10,15 @@ namespace ConsoleApp
     {
         public string Name { get; private set; }
         public int Age { get; private set; }
-        public Weapon PlayerWeapon { get; private set; }
-        public Movement PlayerMovement { get; private set; }
+        public Weapon Weapon { get; private set; }
+        public Movement Movement { get; private set; }
 
         public Player(string name, int age, Weapon playerWeapon, Movement playerMovement)
         {
             Name = name;
             Age = age;
-            PlayerWeapon = playerWeapon;
-            PlayerMovement = playerMovement;
-        }
-    }
-
-    class Weapon
-    {
-        public float WeaponCooldown { get; private set; }
-        public int WeaponDamage { get; private set; }
-
-        public Weapon(float weaponCooldown, int weaponDamage)
-        {
-            WeaponCooldown = weaponCooldown;
-            WeaponDamage = weaponDamage;
-        }
-
-        public bool IsReloading()
-        {
-            throw new NotImplementedException();
-        }
-        public void Attack()
-        {
-            //Attack
-        }
-    }
-    class Movement
-    {
-        public float MovementDirectionX { get; private set; }
-        public float MovementDirectionY { get; private set; }
-        public float MovementSpeed { get; private set; }
-
-        public Movement(float movementDirectionX, float movementDirectionY, float movementSpeed)
-        {
-            MovementDirectionX = movementDirectionX;
-            MovementDirectionY = movementDirectionY;
-            MovementSpeed = movementSpeed;
-        }
-
-        public void Move()
-        {
-            //Do move
+            Weapon = playerWeapon;
+            Movement = playerMovement;
         }
     }
 }
