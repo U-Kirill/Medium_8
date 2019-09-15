ba3bb307f11bcd7a1789971f4763cfa044617eb5
kglad67@gmail.com <PRo1001111Xo309p>
2019-09-15 03:47:26 +0300

    Base functions

--- Lesson1/ConsoleApp/ConsoleApp/Program.cs
+++ Lesson1/ConsoleApp/ConsoleApp/Program.cs
@@ -10,6 +10,55 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            List<ConsoleElement> consoleElements = new List<ConsoleElement>();
+
+            Console.CursorVisible = false;
+
+
+            Cursor cursor = new Cursor(Console.BufferWidth / 2, 10);
+            ClickProcessor clickProcessor = new ClickProcessor();
+
+            // ConsoleElement consoleElement = new ConsoleElement(new Checkbox(),25, 25, 3, 3);
+
+            
+
+            consoleElements.Add(new ConsoleElement(new InputField("Input Field"), 25, 25, 8, 4));
+
+            while (true)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        cursor.MoveLeft();
+                        break;
+
+                    case ConsoleKey.UpArrow:
+                        cursor.MoveUp();
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        cursor.MoveRight();
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        cursor.MoveDown();
+                        break;
+
+                    case ConsoleKey.Enter:
+                        clickProcessor.Click(cursor, consoleElements);
+                        break;
+                }
+
+                Console.Clear();
+                foreach (ConsoleElement item in consoleElements)
+                {
+                    ElementPainter.Paint(item);
+                }
+                
+                CursorPainter.Paint(cursor);
+            }
         }
     }
 }
