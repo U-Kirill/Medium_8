200e95ed4d7b4043ec7471a26a66661fcd9bd5aa
kglad67@gmail.com <PRo1001111Xo309p>
2019-09-16 04:01:06 +0300

    Task 2 complite

--- Lesson1/ConsoleApp/ConsoleApp/Program.cs
+++ Lesson1/ConsoleApp/ConsoleApp/Program.cs
@@ -10,6 +10,21 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            List<Product> products = new List<Product>
+            {
+                new Product(0,"first",80, 2),
+                new Product(1,"second",120, 50),
+                new Product(2,"third",20, 1000),
+                new Product(3,"fourth",300, 0),
+                new Product(3,"fifth",95, 6)
+            };
+
+            foreach (var item in products.Where(product => product.Price < 100 && product.Count > 5))
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadKey();
         }
     }
+
 }
