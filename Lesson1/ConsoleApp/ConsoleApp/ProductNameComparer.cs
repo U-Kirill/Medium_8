f306dc6c1f18c80f1eb909f1a2ede2e1fab0d8a0
kglad67@gmail.com <PRo1001111Xo309p>
2019-09-16 03:35:15 +0300

    Transfer to old project

---
+++ Lesson1/ConsoleApp/ConsoleApp/ProductNameComparer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Lesson3
+{
+    class ProductNameComparer : IComparer<Product>
+    {
+        public int Compare(Product product1, Product product2)
+        {
+            return product1.Name.CompareTo(product2.Name);
+        }
+    }
+}
