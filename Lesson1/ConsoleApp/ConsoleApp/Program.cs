2e0225f3c090eae5e06a2759574345bb662ba255
kglad67@gmail.com <PRo1001111Xo309p>
2019-09-16 05:30:18 +0300

    Task 3 Complited

--- Lesson1/ConsoleApp/ConsoleApp/Program.cs
+++ Lesson1/ConsoleApp/ConsoleApp/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
@@ -10,6 +9,11 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            Network.SendRequestAsync("http://ijunior.ru/", (http) => Console.WriteLine(http));
+            Console.WriteLine("Request sended");
+            Console.ReadKey();
         }
     }
+    delegate void Callback(string html);
+    
 }
